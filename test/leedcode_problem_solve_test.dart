3c436af57eb32f04521137cbd94597f57f8094cd
rustamjon <r.mullaquziyev@uic.group>
2024-11-10 05:12:02 +0500

    Initial Comment

---
+++ test/leedcode_problem_solve_test.dart
@@ -0,0 +1,8 @@
+import 'package:leedcode_problem_solve/leedcode_problem_solve.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
